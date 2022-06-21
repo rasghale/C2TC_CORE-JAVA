931e3deeb185536da996860be054d0ec2aba0ef6
rasghale <rasghale1234@gmail.com>
2022-06-21 13:24:28 +0530

    Hi there

---
+++ Day13/src/com/acharya/inheritance/SingleInheritance.java
@@ -0,0 +1,22 @@
+package com.acharya.inheritance;
+class breath{
+	public void inout() {
+		System.out.println("Animals breath in and out.");
+	}
+}
+class Dog extends breath{
+  public void bark() {
+	System.out.println("Animals can bark");
+	}
+}
+public class SingleInheritance {
+
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+breath a=new breath();
+a.inout();
+a.();
+
+	}
+
+}
