aafacf25e5b18b4b98b8e6bf2b92c4e36b25e489
rasghale <rasghale1234@gmail.com>
2022-06-23 23:29:14 +0530

    Push

---
+++ Day03/src/com/acharya/operators/OperatorExample.java
@@ -0,0 +1,13 @@
+package com.acharya.operators;
+
+
+	public class OperatorExample{  
+		public static void main(String args[]){  
+		int x=10;  
+		System.out.println(x++);
+		System.out.println(++x);
+		System.out.println(x--);
+		System.out.println(--x);
+		}
+	}
+
