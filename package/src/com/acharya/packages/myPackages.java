ea2dc4946fa9251a93d62dbf99b3565f3371f1b1
rasghale <rasghale1234@gmail.com>
2022-08-09 14:26:38 +0530

    commit

---
+++ package/src/com/acharya/packages/myPackages.java
@@ -0,0 +1,18 @@
+package com.acharya.packages;
+
+public class myPackages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String name="Welcome to Avatar World";
+myPackages a=new myPackages();
+a.getNames(name);
+System.out.println(name);	
+	}
+
+	private void getNames(String name) {
+		// TODO Auto-generated method stub
+	
+	}
+
+}
