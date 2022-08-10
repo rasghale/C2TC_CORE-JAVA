318f23f131a9dc3502dbc9db81856f85ac5f3f8b
rasghale <rasghale1234@gmail.com>
2022-08-10 23:16:49 +0530

    commit

---
+++ Polymorphism/src/com/acharya/polymorphism/TestSuper.java
@@ -0,0 +1,19 @@
+package com.acharya.polymorphism;
+class Animal{  
+String color="white";  
+}  
+class Dog extends Animal{  
+String color="black";  
+void printColor(){  
+System.out.println(color);//prints color of Dog class  
+System.out.println(super.color);//prints color of Animal class  
+}  
+}  
+class TestSuper{  
+public static void main(String args[]){  
+Dog d=new Dog();  
+d.printColor();  
+}}  
+
+//black
+//white
