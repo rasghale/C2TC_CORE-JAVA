7a3afd1abe99dccd6ad718b66e09f83b9b4a8d18
rasghale <rasghale1234@gmail.com>
2022-08-12 14:38:24 +0530

    commit

---
+++ stringinbuiltmethods/src/com/acharya/stringinbuiltmethods/Replace.java
@@ -0,0 +1,12 @@
+package com.acharya.stringinbuiltmethods;
+
+public class Replace {
+	public static void main(String args[]) {
+		String s1="Negative thoughts";
+		String replaceString=s1.replace("Negative", "Positive");
+	System.out.println(replaceString);
+		}
+
+}
+//Nelative thoulhts 
+//String should have"" but for char''
---
+++ stringinbuiltmethods/src/com/acharya/stringinbuiltmethods/testSubstring.java
@@ -0,0 +1,12 @@
+package com.acharya.stringinbuiltmethods;
+
+public class testSubstring {
+	public static void main(String args[]) {
+	String s="Ras Bahadur";
+	System.out.println(s.substring(0,3));
+	System.out.println(s.substring(4,7));
+	}
+
+}
+//Ras
+//Bah
