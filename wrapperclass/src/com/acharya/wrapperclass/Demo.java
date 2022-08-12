64fab66f17f7060c47228c54fb476d12da0cb43e
rasghale <rasghale1234@gmail.com>
2022-08-12 12:24:43 +0530

    commit

---
+++ wrapperclass/src/com/acharya/wrapperclass/Demo.java
@@ -0,0 +1,13 @@
+package com.acharya.wrapperclass;
+
+public class Demo {
+	public static void main(String args[])
+	{
+		int a=20;
+		Integer i=Integer.valueOf(a);
+		Integer j=a;
+		System.out.println(a+" "+" "+i+""+j);
+	}
+
+}
+//20  2020 (op)autoboxing
