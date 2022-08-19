4c20879506490519531f81d2f3ee3b2f66ce1b51
rasghale <rasghale1234@gmail.com>
2022-08-19 21:55:28 +0530

    cc

---
+++ Oops/src/com/acharya/oops/Student.java
@@ -0,0 +1,15 @@
+package com.acharya.oops;
+
+public class Student {
+	int id;
+	String name;
+
+	public static void main(String[] args) {
+		Student s1=new Student();
+		System.out.println(s1.id);
+System.out.println(s1.name);	
+}
+
+}
+/*The Exception Handling in Java is one of the powerful mechanism to handle the runtime errors so that the normal flow of the application can be maintained.*/
+/*The core advantage of exception handling is to maintain the normal flow of the application.*/
