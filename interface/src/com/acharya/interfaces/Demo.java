942b93fb1e32137720fc63f5cea73a4b80677eec
rasghale <rasghale1234@gmail.com>
2022-08-21 21:30:55 +0530

    cc

--- interface/src/com/acharya/interfaces/Demo.java
+++ interface/src/com/acharya/interfaces/Demo.java
@@ -1,4 +1,5 @@
 package com.acharya.interfaces;
+import java.io.*;
 interface Ras{
 	final int id=10;
 	void display();
@@ -11,6 +12,9 @@ public class Demo implements Ras {
 	public static void main(String[]args) {
 Demo d=new Demo();
 d.display();
-System.out.println(d);
+System.out.println(id);
 }
 }
+/*Ras 
+10
+*/
